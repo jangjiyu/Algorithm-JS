8e4823182f52860390470a84bc306a1e55dd08fd
JiyuJang <apryll7995@gmail.com>
2022-07-19 01:13:47 +0900

    fix Day3-Q17 (if/else optimization)

--- Day3/Q17.js
+++ Day3/Q17.js
@@ -30,13 +30,54 @@ function solution(s) {
 console.log(solution(s));   // F
 console.log(solution(s2));  // T
 console.log(solution(s3));  // F
-console.log(solution(s4));  // T
+console.log(solution(s4));  // F
 
 
 
-
-// e를 제곱으로 인식해서 틀림
+// if문 정리
 function solution2(s) {
+  let answer;
+  if( !(s.length === 4 || s.length === 6) ) {
+    answer = false;
+    return answer;
+  } 
+  if(isNaN(s) || s.includes('e')) {
+    answer = false;
+  } else {
+    answer = true;
+  }
+  return answer;
+}
+
+console.log(solution2(s));   // F
+console.log(solution2(s2));  // T
+console.log(solution2(s3));  // F
+console.log(solution2(s4));  // F
+
+
+
+
+// if문 정리2
+function solution3(s) {
+  let answer;
+  if( !(s.length === 4 || s.length === 6) ) {
+    answer = false;
+    return answer;
+  } 
+  return ( isNaN(s) || s.includes('e') ) ? answer = false :   answer = true;
+}
+
+console.log(solution3(s));   // F
+console.log(solution3(s2));  // T
+console.log(solution3(s3));  // F
+console.log(solution3(s4));  // F
+
+
+
+
+
+// 아래는 e를 제곱으로 인식해서 틀림
+function solution4(s) {
   let answer;
   if(s.length === 4 || s.length === 6) {
     if(isNaN(s)) {
@@ -50,8 +91,8 @@ function solution2(s) {
   return answer;
 }
 
-console.log(solution2(s));    // F
-console.log(solution2(s2));   // T
-console.log(solution2(s3));   // F
-console.log(solution2(s4));   // T - 1e22를 1의 22승(숫자)으로 인식해서 true 반환.
+console.log(solution4(s));    // F
+console.log(solution4(s2));   // T
+console.log(solution4(s3));   // F
+console.log(solution4(s4));   // T - 1e22를 1의 22승(숫자)으로 인식해서 true 반환.
 
